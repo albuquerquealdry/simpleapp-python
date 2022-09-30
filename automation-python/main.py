ca2f2f09b439bc478f98bc6f7e7fa06ede21899c
albuquerquealdry <albuquerquealdry@gmail.com>
2022-09-30 13:56:17 -0300

    add. entrega

--- automation-python/main.py
+++ automation-python/main.py
@@ -6,13 +6,54 @@ def acctionManagement():
     acction = questionary.select(
             "Tempest Script Managemente Resource AWS 🌴 💚 ",
             choices=[
-                'List  All Resources',
-                'Set EC2',
-                'List Bucket'
+                'List Resources',
                 'Exit'
             ]).ask()
+            
     if (acction == "List Resources"):
-        print(os.system("aws resourcegroupstaggingapi get-resources --region us-east-1")
-
-    
+        
+        region = questionary.select(
+            "Select You Region 🌴 💚 ",
+            choices=[
+                "us-east-1",
+                "us-west-1",
+                "us-west-2",
+                "af-south-1",
+                "ap-east-1",
+                "ap-southeast-3",
+                "ap-south-1",
+                "ap-northeast-3",
+                "ap-northeast-2",
+                "ap-southeast-1",
+                "ap-southeast-2",
+                "ap-northeast-1",
+                "ca-central-1",
+                "eu-central-1",
+                "eu-west-1",
+                "eu-west-2",
+                "eu-south-1",
+                "eu-west-3",
+                "eu-north-1",
+                "me-south-1",
+                "me-central-1",
+                "sa-east-1",
+                "us-gov-east-1",
+                "us-gov-west-1"
+            ]).ask()
+        resource = questionary.select(
+            "Select Resource 🌴 💚 ",
+            choices=[
+                'Bucket',
+                'EC2',
+                'EKS',
+                'ALL'
+            ]).ask()
+        if (resource == "EKS"):
+            os.system(f"aws eks list-clusters --region {region}")
+        if (resource == "EC2"):
+            os.system(f"aws ec2 describe-instances --region {region}")
+        if (resource == "Bucket"):
+            os.system(f"aws s3api list-buckets  --region {region}")
+        if (resource == "ALL"):
+            os.system(f" aws resourcegroupstaggingapi get-resources --region {region}")
 acctionManagement()
--- automation-python/readme.md
+++ automation-python/readme.md
@@ -1,1 +1,15 @@
 ## Script de Automatização de recursos
+
+- Criei uma CLI que espera comandos em Script para listar e trazer direto da AWS os recursos.
+- Para executar o script é só instalar o python3 e a biblioteca questionary com o seguinte comando.
+~~~javascript
+pip install questionary
+~~~
+- Para executar é só executar:
+~~~javascript
+python3 main.py
+~~~
+- O script te dará um menu alternativo que você escolhe o recurso que quer que ele retorne na região indicada também por você!
+
+* OBS:
+- O script só trará os recursos caso você esteja logado na CLI da AWS.
